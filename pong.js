69c95ee178a4add00dd2a0b6ee577770ffbcc960
Kristin Herrmann <dickmann.michael@gmail.com>
2018-11-17 21:29:59 -0700

    Net, ball, and score

---
+++ pong.js
@@ -0,0 +1,83 @@
+const canvas = document.getElementById("pong");
+var context = canvas.getContext("2d");
+
+
+context.fillStyle = "black";
+context.fillRect(100, 200, 50, 75);
+context.fillStyle="red";
+context.beginPath();
+context.arc(300, 350, 100, 0, Math.PI*2, false);
+context.closePath();
+context.fill();
+
+function drawRect(x, y, w, h, color) { //this is the first of three draw functions
+    context.fillStyle = color;
+    context.fillRect(x, y, w, h);
+}
+
+function drawCircle(x, y, r, color) { //this is the 2nd of three draw functions
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(x, y, r, 0, Math.PI*2, false);
+    context.closePath();
+    context.fill();
+}
+
+function drawText(text, x, y, color) { //this is the 3rd of three draw functions
+    context.fillStyle = color;
+    context.font = "75px fantasy";
+    context.fillText(text, x, y);
+}
+
+let rectX = 0;
+
+function render(){
+    drawRect(0, 0, 600, 400, "black");
+    drawRect(rectX, 100, 100, 500, "red");
+    rectX = rectX + 100;
+}
+
+setInterval(render, 1000);
+
+const user = {
+    x : 0,
+    y : canvas.height/2 - 100/2,
+    width : 10,
+    height : 100,
+    color : "WHITE",
+    score : 0
+}
+
+const com = {
+    x : canvas.width - 10,
+    y : canvas.height/2 - 100/2,
+    width : 10,
+    height : 100,
+    color : "WHITE",
+    score : 0,
+}
+
+const net = {
+    x : canvas.width/2 - 2/2,
+    y : 0,
+    width : 2,
+    height: 10,
+    color : "WHITE",
+}
+
+const ball = {
+    x : canvas.width/2,
+    y : canvas.height/2,
+    radius : 10,
+    color: "WHITE"
+}
+
+function drawNet() {
+    for(let i = 0; i < canvas.height; i+=15) {
+        drawRect(net.x, net.y + i, net.width, net.height, net.color);
+    }
+}
+
+drawCircle(ball.x, ball.y, ball.r, ball.color);
+drawText(user.score, canvas.width/4, canvas.height/5, "WHITE");
+drawText(com.score, 3*canvas.width/4, canvas.height/5, "WHITE");
